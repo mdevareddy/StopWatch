e250bd89bf1738a6d6ef75947658131be5512dc9
mdevareddy <mdevareddy77@gmail.com>
2024-07-12 16:48:22 +0530

    first commit

---
+++ stopwatch/src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>StopWatch</h1>
+      <h3>Time:</h3>
+      <button>Start</button>
+      <button>Reset</button>
+  
+    </div>
+  );
+}
+
+export default App;
